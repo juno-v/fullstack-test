244cd4850d0390af0e2de4a18a37de41761622bd
Juno Vue <Juno.Vue@Hotmail.com>
2022-05-25 14:41:24 -0500

    able to find matching name for select click, not key down.

--- src/WizardAutoComplete/WizardAutoComplete.js
+++ src/WizardAutoComplete/WizardAutoComplete.js
@@ -1,19 +1,30 @@
 import * as React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-import DataGrid from "../DataGrid/DataGrid";
 
 export default function ComboBox(props) {
-  const [wizardIsSelected, setWizardIsSelected] = React.useState(false);
   const handleFindWizardChange = (event) => {
-    setWizardIsSelected(!wizardIsSelected);
-    // console.log(props.allWizards);
-    // TO DO : find the name, concat it into all lower case and put it into @hogwarts email, return object matching that and delete using that ID
+    let name = "";
+    if (event.type === "click") {
+      console.log(event.target.innerHTML);
+      name = event.target.innerText.toLowerCase().trim();
+    }
+    if (event.type === "keydown") {
+      console.log(`defaultValue`, event.target.value);
+      console.log("keydown");
+    }
+    const findMatchingNameID = props.allWizards.find(
+      (wizard) =>
+        wizard.first_name.toLowerCase() +
+          " " +
+          wizard.last_name.toLowerCase() ===
+        name
+    )?.id;
+    console.log(findMatchingNameID);
   };
   return (
     <>
       <Autocomplete
-        name={"hello"}
         disablePortal
         onChange={(event) => handleFindWizardChange(event)}
         options={props.allWizards.map((wizard) => {
@@ -22,11 +33,6 @@ export default function ComboBox(props) {
         sx={{ width: 300 }}
         renderInput={(params) => <TextField {...params} label="Wizard Names" />}
       />
-      {/* {wizardIsSelected ? (
-        <DataGrid wizards={} />
-      ) : (
-        <div> no wizard found</div>
-      )} */}
     </>
   );
 }
